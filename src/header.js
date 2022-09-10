902d7d0773b034f017cc43e646d408ff9bcd3eb7
amy liu <amyliu45@gmail.com>
2022-09-09 20:45:45 -0600

    add game header

---
+++ src/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div className='header'>
+                {/*<span className='score'>{`SCORE: ${this.props.score}`}</span>*/}
+                <span className='menu-buttons'>
+                    <button>Pause</button>
+                    <button>Shuffle Tiles</button>
+                    <button type={'reset'}>New Game</button>
+                </span>
+                <text color="black">{"SELECTED: " + this.props.selected}</text>
+            </div>
+        )
+    }
+}
