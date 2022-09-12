e2428ff7f8faf4ef97f522b7a113fb13af1e355a
amy liu <amyliu45@gmail.com>
2022-09-12 14:40:52 -0600

    use tile image for render

--- src/tile.js
+++ src/tile.js
@@ -14,6 +14,25 @@ const COLOR = {
     GREY: "rgb(187,187,187)"
 }
 
+const IMAGES = [require('./images/tile-01.png'), require('./images/tile-02.png'),
+    require('./images/tile-03.png'), require('./images/tile-04.png'),
+    require('./images/tile-05.png'), require('./images/tile-06.png'),
+    require('./images/tile-07.png'), require('./images/tile-08.png'),
+    require('./images/tile-09.png'), require('./images/tile-10.png'),
+    require('./images/tile-11.png'), require('./images/tile-12.png'),
+    require('./images/tile-13.png'), require('./images/tile-14.png'),
+    require('./images/tile-15.png'), require('./images/tile-16.png'),
+    require('./images/tile-17.png'), require('./images/tile-18.png'),
+    require('./images/tile-19.png'), require('./images/tile-20.png'),
+    require('./images/tile-21.png'), require('./images/tile-22.png'),
+    require('./images/tile-23.png'), require('./images/tile-24.png'),
+    require('./images/tile-25.png'), require('./images/tile-26.png'),
+    require('./images/tile-27.png'), require('./images/tile-28.png'),
+    require('./images/tile-29.png'), require('./images/tile-30.png'),
+    require('./images/tile-31.png'), require('./images/tile-32.png'),
+    require('./images/tile-33.png'), require('./images/tile-34.png'),
+    require('./images/tile-35.png'), require('./images/tile-36.png'),];
+
 const TILE_WIDLEN = TILE_DIM * 12 / 10;
 
 export class Tile extends React.Component {
@@ -24,57 +43,38 @@ export class Tile extends React.Component {
             return this.renderInvisibleTile();
         }
         if (this.props.selected === this.props.index) {
-            return this.renderSelectedTile();
+            return this.renderVisibleTile("tile-selected");
         }
-        return this.renderUnselectedTile();
+        return this.renderVisibleTile("tile");
     }
 
     renderInvisibleTile() {
         return (
             <svg className="tile-invisible"
-                 style={this.props.style}
                  width={TILE_WIDLEN + "px"}
                  height={TILE_WIDLEN + "px"}>
             </svg>);
     }
 
-    renderSelectedTile() {
+    renderVisibleTile(className) {
         return (
-            <svg className="tile-selected"
+            <svg className={className}
+                 key={this.props.index}
                  style={this.props.style}
                  width={TILE_WIDLEN + "px"}
                  height={TILE_WIDLEN + "px"}
                  onClick={() => {
                      this.props.handler(this.props.index);
                  }}>
-                <image href={require("./images/temp-tile.png")}
+                <image className={this.props.index + this.props.type}
+                       href={IMAGES[this.props.type]}
                        style={this.props.style}
                        width={TILE_WIDLEN + "px"}
                        height={TILE_WIDLEN + "px"}
                        onClick={() => {
                            this.props.handler(this.props.index);
                        }}/>
-                <text x="0" y="30" fill={Color.GREY}>{this.props.right}</text>
-            </svg>);
-    }
-
-    renderUnselectedTile() {
-        return (
-            <svg className="tile"
-                 style={this.props.style}
-                 width={TILE_WIDLEN + "px"}
-                 height={TILE_WIDLEN + "px"}
-                 onClick={() => {
-                     this.props.handler(this.props.index);
-                 }}>
-                <image href={require("./images/temp-tile.png")}
-                       style={this.props.style}
-                       width={TILE_WIDLEN + "px"}
-                       height={TILE_WIDLEN + "px"}
-                       onClick={() => {
-                           this.props.handler(this.props.index);
-                       }}/>
-                <text x={TILE_WIDLEN / 2} y={TILE_WIDLEN / 2} fill={Color.RED}>{this.props.right}</text>
+                {/*<text x={TILE_WIDLEN / 2} y={TILE_WIDLEN / 2} fill={COLOR.YELLOW}>{this.props.index}</text>*/}
             </svg>
         );
     }
